1c04814af216637e47f5c9e0ca01e14908823cbd
Richard Uzsak <richard.uzsak@siemens-healthineers.com>
2021-09-03 22:58:54 +0200

    used Header component in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,9 @@
 import React from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-      root: {
-        flexGrow: 1,
-      }
-    }),
-);
+import Header from "./components/Header";
 
 function App() {
-  const classes = useStyles();
-
   return (
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar variant="dense">
-            <Typography variant="h6" color="inherit">
-              George FE Test
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </div>
+        <Header/>
   );
 }
 
