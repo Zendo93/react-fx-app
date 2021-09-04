1bf43f63bd963d53e9e31d55491843b245986546
Richard Uzsak <richard.uzsak@siemens-healthineers.com>
2021-09-04 10:40:20 +0200

    used SearchBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
-        <Header/>
+        <>
+          <Header/>
+          <SearchBar/>
+        </>
   );
 }
 
