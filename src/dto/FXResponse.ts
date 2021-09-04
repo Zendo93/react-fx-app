2ef32fc4bc77ca2fab19eaff624ba953a3291e1c
Richard Uzsak <richard.uzsak@siemens-healthineers.com>
2021-09-04 13:35:27 +0200

    created FXResponse dto

---
+++ src/dto/FXResponse.ts
@@ -0,0 +1,48 @@
+export interface IFXResponse {
+    institute: number;
+    lastUpdated: string;
+    comparisonDate: string;
+    baseCurrency: string;
+    fx: Array<IForeignExchange>;
+}
+
+interface IForeignExchange {
+    currency: string;
+    precision: number;
+    nameI18N: string;
+    exchangeRate: IExchangeRate;
+    banknoteRate: IBankNoteRate;
+    flags: Array<string>;
+}
+
+interface IExchangeRate {
+    buy: number;
+    middle: number;
+    sell: number;
+    indicator: number;
+    lastModified: string;
+}
+
+interface IBankNoteRate {
+    buy: number;
+    middle: number;
+    sell: number;
+    indicator: number;
+    lastModified: string;
+}
+
+export class FXResponse implements IFXResponse {
+    institute: number;
+    lastUpdated: string;
+    comparisonDate: string;
+    baseCurrency: string;
+    fx: Array<IForeignExchange>;
+
+    constructor(fxResponse: IFXResponse) {
+        this.institute = fxResponse.institute;
+        this.lastUpdated = fxResponse.lastUpdated;
+        this.comparisonDate = fxResponse.comparisonDate;
+        this.baseCurrency = fxResponse.baseCurrency;
+        this.fx = fxResponse.fx;
+    }
+}
