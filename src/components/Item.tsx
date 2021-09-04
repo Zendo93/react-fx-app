107a80be997a51f5138df65f27c7f91fd7d382b3
Richard Uzsak <richard.uzsak@siemens-healthineers.com>
2021-09-04 11:55:28 +0200

    created Item component

---
+++ src/components/Item.tsx
@@ -0,0 +1,42 @@
+import { Card, CardContent, makeStyles, Typography } from "@material-ui/core";
+import React from "react";
+
+const useStyles = makeStyles({
+    root: {
+        minWidth: 275,
+        margin: '24px'
+    },
+    flag: {
+        display: 'inline',
+    },
+    name: {
+        display: 'inline',
+        marginLeft: 30
+    },
+    currency: {
+        display: 'inline',
+        float: 'right'
+    },
+});
+
+function Item() {
+    const classes = useStyles();
+
+   return (
+       <Card className={classes.root} variant="outlined">
+        <CardContent>
+            <Typography className={classes.flag} color="textSecondary" gutterBottom>
+                Flag
+            </Typography>
+            <Typography className={classes.name} variant="h4" component="h2">
+                USD
+            </Typography>
+            <Typography className={classes.currency} variant="h4" component="h2">
+                1.05 EUR
+            </Typography>
+        </CardContent>
+    </Card>
+   );
+}
+
+export default Item;
