3c440817b6664c5c061465403b968d7be97c49af
Richard Uzsak <richard.uzsak@siemens-healthineers.com>
2021-09-04 11:55:52 +0200

    created Result component

---
+++ src/components/Result.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Item from "./Item";
+
+function Result() {
+    return (
+        <>
+            <Item/>
+            <Item/>
+            <Item/>
+            <Item/>
+            <Item/>
+            <Item/>
+            <Item/>
+            <Item/>
+            <Item/>
+        </>
+    );
+}
+
+export default Result;
