8780130a55c5a0fab60d86e6bf5dc335a2c69c0d
Richard Uzsak <richard.uzsak@siemens-healthineers.com>
2021-09-04 10:39:55 +0200

    styled Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,23 @@
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, makeStyles, Toolbar, Typography } from "@material-ui/core";
 import React from "react";
 
+const useStyles = makeStyles({
+    appBar: {
+        height: '4em'
+    },
+    toolbar: {
+        height: 'inherit'
+    }
+});
+
 function Header() {
+
+    const classes = useStyles();
+
     return (
-        <AppBar position="static">
-            <Toolbar variant="dense">
-                <Typography variant="h6" color="inherit">
+        <AppBar className={classes.appBar} position="static">
+            <Toolbar className={classes.toolbar} variant="dense">
+                <Typography variant="h4" color="inherit">
                     George FE Test
                 </Typography>
             </Toolbar>
