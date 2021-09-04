58d82de3c50eff60a84089d810c6774904ece29d
Richard Uzsak <richard.uzsak@siemens-healthineers.com>
2021-09-04 22:38:15 +0200

    used flag from url

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -4,7 +4,6 @@ import {useAppDispatch, useAppSelector} from "../app/hooks";
 import {AppDispatch} from "../app/store";
 import {fetchResult, selectResult} from "../reducers/resultReducer";
 import {IFXResponse} from "../dto/FXResponse";
-import flag from "../data/flags/us.png";
 
 function Result() {
     const result: IFXResponse | {} = useAppSelector(selectResult);
@@ -19,7 +18,7 @@ function Result() {
 
         if (Object.keys(result).length){
           items = result.fx.filter(fx => fx.currency && fx.exchangeRate?.sell)
-              .map(fx => <Item key={fx.currency} flag={flag} name={fx.currency} currency={fx.exchangeRate.sell}/>);
+              .map(fx => <Item key={fx.currency} flag="/flags/us.png" name={fx.currency} currency={fx.exchangeRate.sell}/>);
         }
 
         return items;
