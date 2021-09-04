e1f99740d5698bd139c0102712dbc4ef97102b01
Richard Uzsak <richard.uzsak@siemens-healthineers.com>
2021-09-04 10:39:12 +0200

    created SearchBar component

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,46 @@
+import {Container, createStyles, InputBase, makeStyles, Theme, Typography } from "@material-ui/core";
+import React from "react";
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        container: {
+            margin: 0,
+            backgroundColor: 'black'
+        },
+        form: {
+            '& > *': {
+                margin: theme.spacing(1),
+                width: '25ch',
+            },
+            width: 'fit-content',
+            paddingLeft: '24px',
+        },
+        inputBase: {
+            backgroundColor: 'white',
+            paddingLeft: '0.5em'
+        },
+        typography: {
+            display: 'inline',
+            color: 'white',
+            marginLeft: 0
+        }
+    }),
+);
+
+function SearchBar() {
+
+    const classes = useStyles();
+
+    return (
+        <Container className={classes.container} disableGutters={true} maxWidth={false}>
+            <form className={classes.form} noValidate autoComplete="off">
+                <Typography className={classes.typography} variant="h6">
+                    Search
+                </Typography>
+                <InputBase id="search-input" className={classes.inputBase} />
+            </form>
+        </Container>
+    );
+}
+
+export default SearchBar;
