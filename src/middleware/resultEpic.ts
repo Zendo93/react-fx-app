95600f51e3f3be5f53f77fcdb30a453ff5cc1ac4
Richard Uzsak <richard.uzsak@siemens-healthineers.com>
2021-09-05 18:13:29 +0200

    changed filter to ofType

--- src/middleware/resultEpic.ts
+++ src/middleware/resultEpic.ts
@@ -1,12 +1,13 @@
 import {from, Observable} from "rxjs";
-import {filter, map, mergeMap} from "rxjs/operators";
+import {map, mergeMap} from "rxjs/operators";
 import {getFXResponse} from "../api/foreignExchangeAPI";
 import {setResult} from "../reducers/resultReducer";
 import {Action} from "@reduxjs/toolkit";
+import {ofType} from "redux-observable";
 
 export function resultEpic(action$: Observable<Action>): Observable<Action> {
     return action$.pipe(
-        filter(action => action.type === 'result/fetchResult'),
+        ofType('result/fetchResult'),
         mergeMap( _ => from(getFXResponse("https://run.mocky.io/v3/c88db14a-3128-4fbd-af74-1371c5bb0343")).pipe(
             map(fxResponse => setResult(fxResponse))
         ))
