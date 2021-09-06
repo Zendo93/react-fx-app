9c557bd56ecdaafd9081e5f3d9952613e96b20ee
Richard Uzsak <richard.uzsak@siemens-healthineers.com>
2021-09-06 20:20:52 +0200

    created searchEpic

---
+++ src/middleware/searchEpic.ts
@@ -0,0 +1,19 @@
+import {Observable} from "rxjs";
+import {Action} from "@reduxjs/toolkit";
+import {ofType, StateObservable} from "redux-observable";
+import {map} from "rxjs/operators";
+import {RootState} from "../app/store";
+import {IForeignExchange} from "../dto/FXResponse";
+import {setSearchResult} from "../reducers/searchReducer";
+
+type SearchAction = { payload: string, type: string };
+
+export function searchEpic(action$: Observable<SearchAction>, state$: StateObservable<RootState>): Observable<Action> {
+    return action$.pipe(
+        ofType<SearchAction, string, SearchAction>('search/setSearchTerm'),
+        map((action: SearchAction) => {
+            let result: IForeignExchange[] = state$.value.resultReducer.result;
+            return setSearchResult(result.filter(fx => fx.currency.startsWith(action.payload)));
+        })
+    );
+}
